6556fd154b0008eeea8e007187a1c041da26658b
ShadowOfTheRedFox <74469156+ShadowoftheRedFox@users.noreply.github.com>
2024-11-28 08:52:57 +0100

    merged all sub menu into the same one

--- scripts/UI/MainMenu.gd
+++ scripts/UI/MainMenu.gd
@@ -1,27 +1,38 @@
 extends VBoxContainer
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-    # grab the keyboard focus on specific button
-    $New.grab_focus()
+	# grab the keyboard focus on specific button
+	$Main/MarginContainer/VBoxContainer/New.grab_focus()
 
 func _on_new_pressed() -> void:
-    pass # Replace with function body.
+	pass # Replace with function body.
 
 
 func _on_continue_pressed() -> void:
-    pass # Replace with function body.
+	pass # Replace with function body.
 
 
 func _on_host_pressed() -> void:
-    Global.goto_scene("res://scenes/UI/HostMenu.tscn")
+	# Hide all menu and show host
+	$Main.hide()
+	$Host.show()
 
 
 func _on_options_pressed() -> void:
-    Global.goto_scene("res://scenes/UI/OptionsMenu.tscn")
+	# Hide all menu and show options
+	$Main.hide()
+	$Options.show()
+
+
+func _on_back_pressed() -> void:
+	# Hide all menu and show main
+	$Options.hide()
+	$Host.hide()
+	$Main.show()
+	pass
 
 
 func _on_quit_pressed() -> void:
-    # quit game
-    get_tree().quit()
+	# quit game
+	get_tree().quit()
