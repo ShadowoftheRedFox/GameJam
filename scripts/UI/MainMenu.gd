2c100dd2ec4c715917dd6b46d7f968bbe7f4bac2
ShadowoftheRedFox <leon.merin@gmail.com>
2024-11-29 21:09:36 +0100

    added multiplayer sub menus

--- scripts/UI/MainMenu.gd
+++ scripts/UI/MainMenu.gd
@@ -17,23 +17,28 @@ func _on_continue_pressed() -> void:
 	pass # Replace with function body.
 	
 
-func _on_host_pressed() -> void:
+func _on_multiplayer_pressed() -> void:
 	# Hide all menu and show host
 	$Main.hide()
-	$Host.show()
+	$Multiplayer.show()
+	$Multiplayer/MarginContainer/Main/VBoxContainer/Join.grab_focus()
 
 
 func _on_options_pressed() -> void:
 	# Hide all menu and show options
 	$Main.hide()
 	$Options.show()
+	$Options/HBoxContainer/Main/VBoxContainer/Sound.grab_focus()
 
 
 func _on_back_pressed() -> void:
 	# Hide all menu and show main
 	$Options.hide()
-	$Host.hide()
+	$Multiplayer.hide()
+	$NewGame.hide()
+	$Continue.hide()
 	$Main.show()
+	$Main/MarginContainer/VBoxContainer/New.grab_focus()
 	pass
 
 
@@ -41,6 +46,12 @@ func _on_quit_pressed() -> void:
 	# quit game
 	get_tree().quit()
 	
+
+##### Sub menu of new game #####
+
+
+##### Sub menu of continue game #####
+
 
 ##### Sub menu of options #####
 func _on_control_pressed() -> void:
