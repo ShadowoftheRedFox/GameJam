a07d5f8de18b90910e43f1af435db8d003b74139
ShadowOfTheRedFox <74469156+ShadowoftheRedFox@users.noreply.github.com>
2024-12-02 15:43:44 +0100

    added sub menu signal

--- scripts/UI/Main/Options/Options.gd
+++ scripts/UI/Main/Options/Options.gd
@@ -5,3 +5,19 @@ signal back_pressed
 
 func _on_back_pressed() -> void:
     back_pressed.emit()
+
+
+func _on_sound_pressed() -> void:
+    if $Margin/HBoxContainer/Sub/Sounds.visible:
+        $Margin/HBoxContainer/Sub/Sounds.hide()
+    else:
+        $Margin/HBoxContainer/Sub/Sounds.show()
+    $Margin/HBoxContainer/Sub/Controls.hide()
+
+
+func _on_control_pressed() -> void:
+    if $Margin/HBoxContainer/Sub/Controls.visible:
+        $Margin/HBoxContainer/Sub/Controls.hide()
+    else:
+        $Margin/HBoxContainer/Sub/Controls.show()
+    $Margin/HBoxContainer/Sub/Sounds.hide()
