04b1b4c6520dfa308000981d19f4c45d45d00b29
ShadowOfTheRedFox <74469156+ShadowoftheRedFox@users.noreply.github.com>
2024-12-05 13:45:01 +0100

    general options

--- scripts/UI/Main/Options/Options.gd
+++ scripts/UI/Main/Options/Options.gd
@@ -6,18 +6,30 @@ signal back_pressed
 func _on_back_pressed() -> void:
     back_pressed.emit()
 
+func hide_all() -> void:
+    $Margin/HBoxContainer/Sub/Controls.hide()
+    $Margin/HBoxContainer/Sub/Sounds.hide()
+    $Margin/HBoxContainer/Sub/Sounds.hide()
 
 func _on_sound_pressed() -> void:
+    hide_all()
     if $Margin/HBoxContainer/Sub/Sounds.visible:
         $Margin/HBoxContainer/Sub/Sounds.hide()
     else:
         $Margin/HBoxContainer/Sub/Sounds.show()
-    $Margin/HBoxContainer/Sub/Controls.hide()
 
 
 func _on_control_pressed() -> void:
+    hide_all()
     if $Margin/HBoxContainer/Sub/Controls.visible:
         $Margin/HBoxContainer/Sub/Controls.hide()
     else:
         $Margin/HBoxContainer/Sub/Controls.show()
-    $Margin/HBoxContainer/Sub/Sounds.hide()
+
+
+func _on_general_pressed() -> void:
+    hide_all()
+    if $Margin/HBoxContainer/Sub/General.visible:
+        $Margin/HBoxContainer/Sub/General.hide()
+    else:
+        $Margin/HBoxContainer/Sub/General.show()
