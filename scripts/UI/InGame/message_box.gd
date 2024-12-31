2e0dd5ea9135766fc9b410aabd62c27b7c52b3bf
ShadowOfTheRedFox <74469156+ShadowoftheRedFox@users.noreply.github.com>
2024-12-31 17:51:41 +0100

    fixed text alignment

--- scripts/UI/InGame/message_box.gd
+++ scripts/UI/InGame/message_box.gd
@@ -1,29 +1,36 @@
+@tool
 class_name MessageBox
 extends MarginContainer
+
+enum Alignment {LEFT, CENTER, RIGHT}
 
 @export var title: String = "This is a title.":
     set(value):
         title = value
         if is_node_ready():
             title_element.text = title
-            
-@export var title_alignment: SizeFlags = Control.SIZE_SHRINK_BEGIN:
-    set(value):
-        title_alignment = value
-        if is_node_ready():
-            title_element.size_flags_horizontal = title_alignment
-            
+            queue_redraw()
+
 @export var content: String = "This is a content.":
     set(value):
         content = value
         if is_node_ready():
             content_element.text = content
+            queue_redraw()
             
-@export var content_alignment: SizeFlags = Control.SIZE_SHRINK_BEGIN:
+@export_enum("Left:0","Center:1","Right:2","Fill:3") var title_alignment: int = HORIZONTAL_ALIGNMENT_LEFT:
+    set(value):
+        title_alignment = value
+        if is_node_ready():
+            title_element.horizontal_alignment = value
+            queue_redraw()            
+            
+@export_enum("Left:0","Center:1","Right:2","Fill:3") var content_alignment: int = HORIZONTAL_ALIGNMENT_LEFT:
     set(value):
         content_alignment = value
         if is_node_ready():
-            content_element.size_flags_horizontal = content_alignment
+            content_element.horizontal_alignment = content_alignment
+            queue_redraw()
             
 
 @onready var title_element: Label = $MarginContainer/VBoxContainer/Title
@@ -36,6 +43,4 @@ func _ready() -> void:
     title_element.size_flags_horizontal = title_alignment
     content_element.size_flags_horizontal = content_alignment
     
-    #title_element.queue_redraw()
-    #content_element.queue_redraw()
     self.queue_redraw()
